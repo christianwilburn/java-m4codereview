e395d427cfcef196c2bd6603b25da0ade7deb04e
christianwilburn <98135173+christianwilburn@users.noreply.github.com>
2025-01-28 20:22:20 -0500

    update to fix infinte loop

--- src/main/java/com/example/Master.java
+++ src/main/java/com/example/Master.java
@@ -3,32 +3,31 @@ import javax.swing.JOptionPane;
 
 public class Master {
 
-    // create showChar method with alpha and digt args
-    static void showChar(char alpha, int digt) {
-        System.out.println(alpha + "- is at position: " + digt);
-    }
+   // create showChar method with alpha and digt args
+   static void showChar(char alpha, int digt) {
+    System.out.println(alpha + "- is at position: " + digt);
+}
 
-    public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog(null, "Enter one word please");
+public static void main(String[] args) {
+    String input = JOptionPane.showInputDialog(null, "Enter one word please");
+
+    while (true) { // Loop to keep asking until valid input is provided
         String num = JOptionPane.showInputDialog(null, "Please enter a number");
+        try {
+            int letr = Integer.parseInt(num);
 
-        // convert num to int format and select that number as char placement
-        int letr = Integer.parseInt(num);
-
-        while (input.length() >= letr) {
-            try {
+            // Check if the number is within bounds
+            if (letr >= 0 && letr < input.length()) {
                 char output = input.charAt(letr);
                 showChar(output, letr);
-                if (input.length() < letr) {
-                    JOptionPane.showMessageDialog(null,
-                            "Please Enter a number equal to or less than the word length! ");
-                }
-            } catch (NumberFormatException e) {
-                JOptionPane.showMessageDialog(null, "Please enter a numerical value!");
+                break; // Exit the loop once a valid number is processed
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "Please enter a number between 0 and " + (input.length() - 1));
             }
-
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Please enter a valid numerical value!");
         }
-
     }
-
+}
 }
