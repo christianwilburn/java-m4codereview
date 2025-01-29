ff8bc7612d8e93d9629d236f6c7eeec129f43059
christianwilburn <98135173+christianwilburn@users.noreply.github.com>
2025-01-28 20:18:36 -0500

    loop add

--- src/main/java/com/example/Master.java
+++ src/main/java/com/example/Master.java
@@ -3,33 +3,32 @@ import javax.swing.JOptionPane;
 
 public class Master {
 
-    //create showChar method with alpha and digt args
+    // create showChar method with alpha and digt args
     static void showChar(char alpha, int digt) {
         System.out.println(alpha + "- is at position: " + digt);
     }
 
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog(null, "Enter one word please");
+        String num = JOptionPane.showInputDialog(null, "Please enter a number");
 
-    public static void main(String[] args) { 
-      String input = JOptionPane.showInputDialog(null, "Enter one word please");
-      String num = JOptionPane.showInputDialog(null, "Please enter a number");
-      
-
-    //convert num to int format and select that number as char placement 
+        // convert num to int format and select that number as char placement
         int letr = Integer.parseInt(num);
-        
-        while ( input.length() >= letr) {
+
+        while (input.length() >= letr) {
             try {
-        char output = input.charAt(letr);
-        showChar(output, letr);
-        if (input.length()< letr) {
-        JOptionPane.showMessageDialog(null, "Please Enter a number equal to or less than the word length! ");
-        }
+                char output = input.charAt(letr);
+                showChar(output, letr);
+                if (input.length() < letr) {
+                    JOptionPane.showMessageDialog(null,
+                            "Please Enter a number equal to or less than the word length! ");
+                }
             } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Please enter a numerical value!");
+                JOptionPane.showMessageDialog(null, "Please enter a numerical value!");
             }
-            
-        }  
-    
+
+        }
+
     }
-    
+
 }
